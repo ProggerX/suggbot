57d04353b0d32cca881d86519369ed88054917cf
ProggerX <x@proggers.ru>
2025-10-16 09:55:14 +0300

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Telegram.Bot.Simple
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs =
+    { flake-parts, nixpkgs, ... }@inputs:
+    let
+      hs-project =
+        {
+          pkgs,
+          isShell ? false,
+        }:
+        pkgs.haskellPackages.developPackage {
+          root = ./.;
+          returnShellEnv = isShell;
+          modifier =
+            drv:
+            pkgs.haskell.lib.addBuildTools drv (
+              with pkgs;
+              [
+                cabal-install
+                haskell-language-server
+              ]
+            );
+        };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.platforms.unix;
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages.default = hs-project { inherit pkgs; };
+          devShells.default = hs-project {
+            inherit pkgs;
+            isShell = true;
+          };
+        };
+    };
+}
